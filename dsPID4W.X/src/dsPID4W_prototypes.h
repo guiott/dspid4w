79abbdf520399ceaa450c4dcb9dcd2df5980046a
guiott@gmail.com <guiott@gmail.com@f5e2ca5d-b412-bdf7-bea0-4af783016937>
2012-11-25 19:52:12 +0000

    Little change

---
+++ dsPID4W.X/src/dsPID4W_prototypes.h
@@ -0,0 +1,40 @@
+/* ///////////////////////////////////////////////////////////////////////// */
+/* Included	in "dsPid_definitions.h", it contains functions prototypes / */
+/* ///////////////////////////////////////////////////////////////////////// */
+
+void ConstantsDefault (void);
+void PidCalc (void);
+void PidLCalc (void);
+void ThetaDesF(float Angle);
+void AdcCalc(void);
+void DelayN1ms(int n);
+void DelayN10us(int n);
+void InitAnglePid(void);
+void Orientation(void);
+void DeadReckoning(void);
+unsigned char IncrCircPtr(int Port);
+void InitPidR(void);
+void InitPidL(void);
+void PidL(void);
+void TxParameters(char TxCmd,int TxCmdLen, int Port);
+void UartTx(void);
+void UartRxError(int Err, int Port);
+unsigned char UartChkSum (unsigned char *Buff, unsigned int BuffSize);
+void Parser (void);
+void UartRx(void);
+void Uart2Rx(void);
+void Settings(void);
+void ISR_Settings(void);
+void UsartSetting(void);
+void Usart2Setting(void);
+void Speed(void);
+// void _ISR _INT1Interrupt(void);
+// void _ISR_PSV _U1RXInterrupt(void);
+// void _ISR _U1TXInterrupt(void);
+// void _ISR_PSV _IC1Interrupt(void);
+// void _ISR_PSV _T1Interrupt(void);
+// void _ISR _CNInterrupt(void);
+
+// void _ISR _ADCInterrupt(void);
+// void _ISR _QEIInterrupt(void);
+
